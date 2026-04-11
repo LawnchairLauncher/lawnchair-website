b099c48880766e32ac6228bc21886be163109358
Pun Butrach <pun.butrach@gmail.com>
2026-04-12 01:16:57 +0700

    chore: Update script

--- scripts/lib/markdown.js
+++ scripts/lib/markdown.js
@@ -10,8 +10,8 @@ import markedFootnote from "marked-footnote";
 
 const rootDir = process.cwd();
 
-const metadataLastModifiedKeys = ["last modified"];
-const metadataFirstPublishedKeys = ["first published"];
+const metadataLastModifiedKeys = ["last-modified"];
+const metadataFirstPublishedKeys = ["first-published"];
 
 let markedInstance;
 
